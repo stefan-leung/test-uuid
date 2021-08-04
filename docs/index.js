610f42c4a13026927b227c57d5da937784f6f948
stef <80367026+stefan-leung@users.noreply.github.com>
2021-08-03 20:04:33 -0700

    DOCS!

--- docs/index.js
+++ docs/index.js
@@ -23,17 +23,47 @@ $.ajax({
 
 console.log(mdjson); 
 
-const body = document.getElementsByTagName('body')[0];
+let body = document.getElementsByTagName('body')[0];
 let navigation = document.createElement('div');
+let content = document.createElement('div');
 var mdDiv
 
 navigation.id = 'nav';
+navigation.classList.add('closebtn');
+navigation.setAttribute('href', 'javascript:void(0)');
+navigation.setAttribute('onClick', 'closeNav()');
 
 mdjson.forEach(markdown => {
-    mdDiv = document.createElement('div');
+    mdDiv = document.createElement('a');
     mdDiv.innerHTML = markdown[0];
+    mdDiv.style.cursor = 'pointer';
+    mdDiv.setAttribute('onClick', `openMd("${markdown[0]}")`)
 
     navigation.appendChild(mdDiv);
 });
 
+let openmenu = document.createElement('span');
+openmenu.innerText = 'Open Menu';
+openmenu.style.fontSize = '30px';
+openmenu.style.cursor = 'pointer';
+openmenu.setAttribute('onClick', 'openNav()');
+
 body.appendChild(navigation);
+body.appendChild(openmenu);
+body.appendChild(content)
+
+function openMd(name) {
+    mdjson.forEach(markdown => {
+        if(markdown[0] == name) {
+            content.innerText = markdown[1];
+        };
+    });
+};
+
+function openNav() {
+    document.getElementById("nav").style.width = "250px";
+};
+
+function closeNav() {
+    document.getElementById("nav").style.width = "0";
+};
